b136499ba2232878e15572016a66eb68c7fdcff7
rarothschild <rarothsc@gmail.com>
2022-11-15 09:13:26 -0800

    spell fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rarothschild.github.io',
-  	base: '/RobIndustires',
+  	base: '/RobIndustries',
 	integrations: [mdx(), tailwind()],
 });
