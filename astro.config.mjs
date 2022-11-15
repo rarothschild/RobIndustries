1ffe6e1bd003fa7f3423e5c7fa9743dc6ca56ca0
rarothschild <rarothsc@gmail.com>
2022-11-14 18:55:18 -0800

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://rarothschild.github.io',
+  	base: '/RobIndustires',
 	integrations: [mdx(), tailwind()],
 });
