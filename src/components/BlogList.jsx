86f657fe9b905dff413890802c61d2f69332cc96
rarothschild <rarothsc@gmail.com>
2022-12-11 23:31:00 -0800

    Update contact panel, some alignment fixes

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -20,7 +20,7 @@ export default function BlogList(props) {
     for (let i = 0; i < uniqueTags.length; i++) {
         tagCoor[uniqueTags[i]] = tagColors[i]
     }
-    
+
     createEffect(() => {
         if (tagSelect()==="All") {
             setShowBlogs(blogs())
@@ -33,46 +33,45 @@ export default function BlogList(props) {
     
     function PostPreview(post) {
         return (
-            <div class="flex flex-col w-72 shadow-lg bg-[#e9e4de]  border-2 border-black">
+            <a href={post.url} class="flex flex-col w-72 mb-2 shadow-[-10px_10px_0px_0px_rgba(0,0,0,0.3)] bg-[#e9e4de]  border-2 border-black">
                 <div class="bg-slate-800 border-b border-black">
                     <img class="object-contain h-52 w-72 mx-auto" src={post.frontmatter.image} alt="Tool logo" />
                 </div>
                 <h3 class="p-2 text-center font-extrabold text-lg bg-slate-400 border-b border-black">{post.frontmatter.title}</h3>
                 <p class="pt-1 text-center font-medium text-lg">{post.frontmatter.pubDate}</p>
                 <p class="p-2 h-full text-center font-medium text-lg">{post.frontmatter.description}</p>
-                <div class="flex flex-row flex-wrap gap-2 p-2 font-bold ">
+                <div class="flex flex-row flex-wrap gap-2 px-2 py-1 font-bold ">
                     {post.frontmatter.categories.map(cat => 
                         <div class="rounded-md p-2 border-2 border-black" style={{"background-color": tagCoor[cat]}}>{cat}</div>)}
                 </div>
-            </div>
+            </a>
         )
     }
 
     function Tag(category) {
         return(
-            <button class="rounded-md p-2 border-2 border-black" style={{"background-color": tagCoor[category]}} onClick={() => setTagSelect(category)}>{category}</button>
+            <button class="rounded-md px-2 py-1 border-2 border-black" style={{"background-color": tagCoor[category]}} onClick={() => setTagSelect(category)}>{category}</button>
         )
     }
 
     return(
-        <div class="">
-            <div class="flex flex-row p-4 bg-[#353038] "><h2 class="basis-1/2 text-center text-3xl font-extrabold text-[#d3cece]">Blog Posts</h2></div>
-            <div class="flex flex-row p-4">
-                <div class="w-56 mr-8 font-bold  bg-slate-500 border-2 border-black">
-                    <div class="p-1 px-3 justify-between text-center bg-[#d3bc8a] border-b border-black">
-                        <h3>Sort By:</h3>
+        <div class="flex flex-row overflow-hidden">
+            <div class="mt-8 flex flex-col p-4 gap-8">
+                <h2 class="text-center text-3xl font-extrabold text-[#d3cece]">Blog Posts</h2>
+                <div class="flex flex-col w-64 p-2 font-bold gap-2 ">
+                    <div class="justify-between text-center">
+                        <button class="px-2 rounded-md bg-red-300 border-2 border-black" onClick={() => setTagSelect("All")}>Sort By Date</button>
                     </div>
-                    <div class="flex flex-row place-content-center p-1 px-3 gap-2 bg-gray-300">
-                        <button class="px-2 rounded-md bg-red-300 border-2 border-black" onClick={() => setTagSelect("All")}>Date</button>
-                        <p class=" self-center">Or Tags:</p>
+                    <div class="flex flex-row place-content-center">
+                        <p class=" self-center text-[#d3cece]">Or Tags:</p>
                     </div>
-                    <div class="flex flex-row flex-wrap gap-2 p-2">
+                    <div class="flex flex-row flex-wrap gap-2 ">
                         {uniqueTags.map(category => Tag(category))}
                     </div>
                 </div>
-                <div class="flex flex-row gap-4">
-                    {showBlogs().map(blog => PostPreview(blog))}
-                </div>
+            </div>
+            <div class="flex flex-row gap-6">
+                {showBlogs().map(blog => PostPreview(blog))}
             </div>
         </div>
     )
