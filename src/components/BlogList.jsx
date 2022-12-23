5fdfc5a5c0cd58038f54ce59e137ec114868928f
rarothschild <rarothsc@gmail.com>
2022-12-22 21:22:17 -0800

    Redoing frontend layout

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -33,17 +33,18 @@ export default function BlogList(props) {
     
     function PostPreview(post) {
         return (
-            <a href={post.url} class="flex flex-col w-72 mb-2 shadow-[-10px_10px_0px_0px_rgba(0,0,0,0.3)] bg-slate-200  border-2 border-black text-black">
-                <div class="bg-slate-800 border-b border-black">
-                    <img class="object-contain h-52 w-72 mx-auto" src={post.frontmatter.image} alt="Tool logo" />
+            <a href={post.url} class="w-80 min-w-[20rem] rounded-md border-2 border-black bg-[color:var(--color-bg)] ">
+                <div class="">
+                    <img class="object-contain h-52 mx-auto " src={post.frontmatter.image} alt="Tool logo" />
                 </div>
-                <h3 class="p-2 text-center font-extrabold text-lg bg-slate-400 border-b border-black">{post.frontmatter.title}</h3>
-                <p class="pt-1 text-center font-medium text-lg">{post.frontmatter.pubDate}</p>
-                <p class="p-2 h-full text-center font-medium text-lg">{post.frontmatter.description}</p>
+                <h3 class="pt-2 text-center font-extrabold text-lg ">{post.frontmatter.title}</h3>
+                <p class="text-center font-medium text-lg">{post.frontmatter.pubDate}</p>
+                <p class="p-2 min-h-[5rem] text-center font-medium text-lg">{post.frontmatter.description}</p>
                 <div class="flex flex-row flex-wrap gap-2 px-2 py-1 font-bold ">
                     {post.frontmatter.categories.map(cat => 
-                        <div class="rounded-md px-2 py-1 border-2 border-black" style={{"background-color": tagCoor[cat]}}>{cat}</div>)}
+                        <div class="rounded-md px-2 py-1 border-2 border-black text-black" style={{"background-color": tagCoor[cat]}}>{cat}</div>)}
                 </div>
+                
             </a>
         )
     }
@@ -55,10 +56,10 @@ export default function BlogList(props) {
     }
 
     return(
-        <div class="flex flex-col md:flex-row overflow-hidden">
-            <div class="mt-8 flex flex-col p-4 gap-8 ">
+        <div class="w-full flex px-4 max-w-[100rem] mx-auto">
+            <div class="basis-1/4 flex flex-col p-4 gap-8 ">
                 <h2 class="text-center text-3xl font-extrabold">Blog Posts</h2>
-                <div class="flex flex-col w-64 p-2 font-bold gap-2 ">
+                <div class="flex flex-col p-2 font-bold gap-2 ">
                     <div class="justify-between text-center">
                         <button class="px-2 rounded-md bg-red-300 border-2 border-black text-black" onClick={() => setTagSelect("All")}>Sort By Date</button>
                     </div>
@@ -70,7 +71,7 @@ export default function BlogList(props) {
                     </div>
                 </div>
             </div>
-            <div class="flex flex-row gap-6 ">
+            <div class="w-full flex gap-6 p-6 overflow-x-scroll scrollbar bg-[color:var(--color-content)]">
                 {showBlogs().map(blog => PostPreview(blog))}
             </div>
         </div>
